8735e82334e777196e5672ff4e3fdb600bdc1e86
prasert1965 <160126067+prasert1965@users.noreply.github.com>
2026-07-26 17:02:30 +0700

    Refactor current_time usage in prediction script
    Commented out the current_time variable and its usage in the prediction function.

--- Deploy EA Alpha thal trait web app.py
+++ Deploy EA Alpha thal trait web app.py
@@ -11,7 +11,7 @@ import streamlit as st
 import datetime
 
 
-current_time = datetime.now()
+#current_time = datetime.now()
 # Access it through the module
 today = datetime.date.today()
 
@@ -30,7 +30,7 @@ def EA_Alpha_thal_prediction(input_data):
 
      prediction = loaded_model.predict(input_data_reshaped)
      print(today)
-     print(current_time)
+    # print(current_time)
      print(input_data_as_numpy_array)
      print(prediction)
 
