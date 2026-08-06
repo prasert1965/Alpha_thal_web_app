ae812a71d1c7bf5250534e644c5d2f3c9bd348f6
prasert1965 <160126067+prasert1965@users.noreply.github.com>
2026-08-06 07:47:44 +0700

    Enhance alpha thalassemia prediction web app
    Updated the web app for predicting alpha thalassemia carrier status. Added a report section and improved user input handling.

--- Deploy EA Alpha thal trait web app.py
+++ Deploy EA Alpha thal trait web app.py
@@ -77,7 +77,7 @@ def main():
          diagnosis = EA_Alpha_thal_prediction([AGE, HCT, HGB, RBC, MCV, MCH, MCHC, RDW])        
         
     st.success(diagnosis)
-      
+    st.write('Report by ...')   
     
 if __name__ == '__main__':
     main() 
