25405a7b3f4e5673853382baad72ab101c6140ea
prasert1965 <160126067+prasert1965@users.noreply.github.com>
2026-08-06 07:51:00 +0700

    Enhance alpha thalassemia prediction web app
    Updated the web app for alpha thalassemia prediction with improved user interface and added functionality for printing the report.

--- Deploy EA Alpha thal trait web app.py
+++ Deploy EA Alpha thal trait web app.py
@@ -77,8 +77,8 @@ def main():
          diagnosis = EA_Alpha_thal_prediction([AGE, HCT, HGB, RBC, MCV, MCH, MCHC, RDW])        
         
     st.success(diagnosis)
-    st.write('Report by ...')   
-    
+    st.write('Report by ..................')   
+    st.write('Approve by Prasert Chantanaskulwong ') 
 if __name__ == '__main__':
     main() 
 #st.title("รายงานผลการทำนาย")
