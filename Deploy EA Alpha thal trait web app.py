02923157a3271135bfae4e66b439c3755c7ea98f
prasert1965 <160126067+prasert1965@users.noreply.github.com>
2026-08-12 07:48:03 +0700

    Refactor and clean up EA Alpha thal trait web app

--- Deploy EA Alpha thal trait web app.py
+++ Deploy EA Alpha thal trait web app.py
@@ -82,8 +82,8 @@ def main():
     
     if st.button('Prediction result Pls. Click'):
          diagnosis = EA_Alpha_thal_prediction([AGE, HCT, HGB, RBC, MCV, MCH, MCHC, RDW])        
-        
-    st.success(diagnosis)
+       st.success(diagnosis)
+    
     col1, col2, col3, = st.columns(3)
     with col1:
         st.write('Predicted by ..Phrae ADA ML.. ') 
