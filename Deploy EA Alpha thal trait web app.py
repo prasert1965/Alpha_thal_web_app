a0ece4b0d823207e5a045d09570f841706e582a5
prasert1965 <160126067+prasert1965@users.noreply.github.com>
2026-08-12 08:04:44 +0700

    Update Deploy EA Alpha thal trait web app.py

--- Deploy EA Alpha thal trait web app.py
+++ Deploy EA Alpha thal trait web app.py
@@ -43,7 +43,7 @@ def EA_Alpha_thal_prediction(input_data):
      if (prediction[0] == 0):
        return 'This person is alpha thalassemia carrier'
      else:
-       return 'This person is not alpha thalassemia carrier'
+        return 'This person is not alpha thalassemia carrier'
      
 def main():
 
