c73a2d383e6f0737fe5ac294ce8a62e8909200d9
prasert1965 <160126067+prasert1965@users.noreply.github.com>
2026-08-20 20:35:45 +0700

    Fix formatting and improve code readability

--- Deploy EA Alpha thal trait web app.py
+++ Deploy EA Alpha thal trait web app.py
@@ -56,11 +56,12 @@ def EA_Alpha_thal_prediction(input_data):
         return 'This person is not alpha thalassemia carrier'
      
 def main():
-    st.markdown(hide_style, unsafe_allow_html=True)
+    
     # giving a title  
     st.title('Web for prediction Alpha Thalassemia carrier')   
     st.write('In 2024, From Phrae Adaboost model on Dataset3 demonstrated acc 97% sen 100% spec 95% AUC 0.974. In 2026, A performance evaluation of the adaboost model in the Khon Kaen population showed acc 63% Sen 93.4% Spec 57.4% PPV 30.9% NPV 97.7% AUC 0.754.')
-
+    st.markdown(hide_style, unsafe_allow_html=True)
+    
     # getting the input data from the user
     col1, col2, col3, col4 = st.columns(4)
     with col1:
