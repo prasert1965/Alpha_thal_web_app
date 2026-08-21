db39b5f02f0bf8372556ad91d20d594a6668ea1a
prasert1965 <160126067+prasert1965@users.noreply.github.com>
2026-08-21 09:44:34 +0700

    Enhance EA Alpha thal trait web app functionality
    Updated the web app to include CSS for hiding elements and improved the prediction function.

--- Deploy EA Alpha thal trait web app.py
+++ Deploy EA Alpha thal trait web app.py
@@ -44,7 +44,7 @@ hide_badge_style = """
 """
 
 # Inject CSS (Must set unsafe_allow_html to True)
-    st.markdown(hide_badge_style, unsafe_allow_html=True)
+    
 tz_thai = pytz.timezone('Asia/Bangkok')
 now_thai = datetime.now(tz_thai)
 current_time_str = now_thai.strftime("%d/%m/%y time %H:%M minute.")
@@ -75,6 +75,7 @@ def EA_Alpha_thal_prediction(input_data):
      
 def main():
     
+    st.markdown(hide_badge_style, unsafe_allow_html=True)
     st.markdown(hide_style, unsafe_allow_html=True)
     
     # giving a title  
